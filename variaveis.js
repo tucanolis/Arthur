449269b77ac48433a89774a71a4cd14c111cf88c
tucanolis <arthur.sinmc@gmail.com>
2024-03-28 11:32:17 -0300

    chore: exercicio com variaveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+var pet = "Cachorro";
+console.log(pet);
+var pet = "Mini pig";
+console.log(pet);
+
+var gatinho = "Persa";
+doguinho = "Caramelo";
+let professor ="Thiago";
+
+console.log(`Meu pet de escolhe é: ${pet} \nA idade dele é ${idadePet} anos`);
+
+
+
+
+
+
+
+
